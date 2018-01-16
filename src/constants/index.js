b850b4ccbd4502d67aa1e940eaaf16cc151c0225
fahmisan <ahmadfahmisan@gmail.com>
2018-01-16 13:06:41 +0800

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export const HOST = 'http://192.168.182.60:3000'
---
+++ src/utils/index.js
@@ -0,0 +1,40 @@
+import { HOST } from '../constants';
+
+export function normalizeRooms(rooms) {
+  return rooms.map(room => {
+    return {
+      id: room.id,
+      title: room.listing_name || '',
+      image: `${HOST}${room.image}` || '',
+      homeType: room.home_type || '',
+      bedRoom: room.bed_room || '',
+      price: room.price || '',
+      instant: room.instant || ''
+    }
+  })
+}
+
+export function normalizeRoom(room) {
+  return {
+    id: room.id,
+    title: room.listing_name || '',
+    image: `${HOST}${room.image}` || '',
+    homeType: room.home_type || '',
+    bedRoom: room.bed_room || '',
+    price: room.price || '',
+    instant: room.instant || '',
+    summary: room.summary || '',
+    accomodate: room.accomodate || '',
+    bathRoom: room.bath_room || '',
+    unavailableDates: room.unavailable_dates || '',
+    host: room.host ? {
+      email: room.host.email || '',
+      fullname: room.host.fullname || '',
+      avatar: room.host.avatar || '',
+    } : {
+      email: '',
+      fullname: '',
+      avatar: ''
+    }
+  }
+}
